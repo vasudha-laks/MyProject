aca47ea153534872de360be777740cb1e6e4d229
vasudha <vasudhalakshmi.koka@gmail.com>
2021-12-16 17:22:54 +0530

    add changes

--- parent.java
+++ parent.java
@@ -1,18 +1,18 @@
 
 public class parent {
-	public void show()
+	public void show1()
 	{
-		System.out.println("parent's show");
+		System.out.println("parent's show1");
 	}
 	class child extends parent{
-	      public void show()
+	      public void show1()
 		{
-			System.out.println("child's show");
+			System.out.println("child's show1");
 		}
 
 			public  void main(String[] args) {
 			parent p=new parent();
-			p.show();
+			p.show1();
 			
 		}
 
